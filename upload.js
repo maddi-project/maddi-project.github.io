65054b8d8af8764bdcb675f79db406680654b42d
maddi-project <69698429+maddi-project@users.noreply.github.com>
2020-08-15 16:26:33 +0200

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,10 +1,4 @@
 function upload() {
-  var imgcanvas = document.getElementById("can");
-  var fileinput = document.getElementById("finput");
-  var image = new SimpleImage(fileinput);
-  image.drawTo(imgcanvas);
-};
-function upload1() {
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
   var img = document.getElementById("finput");
