d53161320350bc51dc06e9c8d92980e6ecb7d8f1
maddi-project <69698429+maddi-project@users.noreply.github.com>
2020-08-15 18:18:52 +0200

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,6 +1,6 @@
 function upload() {
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
-  var img = document.getElementById("tree3.jpg");
+  var img = document.getElementById('display');
   ctx.drawImage(img, 10, 10);
 };
