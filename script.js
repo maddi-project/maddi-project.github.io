8b3ddc26898baf308962a82dff5561cd3ff704e9
maddi-project <69698429+maddi-project@users.noreply.github.com>
2020-08-22 16:41:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ function draw1() {
      	/*var ctxt=canvas.getContext("2d");*/
      	var url = canvas.toDataURL();
       	var newImg = new Image();
-	img.setAttribute('crossOrigin', 'anonymous');
+	newImg.setAttribute('crossOrigin', 'anonymous');
       	newImg.src = url;
       	/*document.body.appendChild(newImg);*/
   var c = document.getElementById('canvas2');
