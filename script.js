5eb99ff3a8046ce1fb1d011c3fcfad86ebdaabc7
maddi-project <69698429+maddi-project@users.noreply.github.com>
2020-09-25 17:23:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -211,11 +211,7 @@ function boxClick(well) {
 		color = '#DA81F5';
 		color3 = '#BF00FF';
 	}
-	/*
-	if (document.getElementById(well).style.backgroundColor !== color && document.getElementById(well).style.backgroundColor !== '') {
-		return		
-	}*/
-	
+
 	var color2;
 	if (text !== "X") {
 		document.getElementById(well).innerHTML = "X";
@@ -525,6 +521,21 @@ function cleartable() {
 }
 
 function show() {
+	for (i = 0; i < 96; i++) {
+		if (document.getElementById('c' + i).style.backgroundColor == 'grey') {
+			document.getElementById('c' + i).style.backgroundColor = '#BDBDBD';
+		} else if (document.getElementById('c' + i).style.backgroundColor == 'red') {
+			ocument.getElementById('c' + i).style.backgroundColor = '#F78181';
+		} else if (document.getElementById('c' + i).style.backgroundColor == 'green') {
+			ocument.getElementById('c' + i).style.backgroundColor = '#81F781';
+		} else if (document.getElementById('c' + i).style.backgroundColor == 'blue') {
+			ocument.getElementById('c' + i).style.backgroundColor = '#81BEF7';
+		} else if (document.getElementById('c' + i).style.backgroundColor == 'yellow') {
+			ocument.getElementById('c' + i).style.backgroundColor = '#F3F781';
+		} else if (document.getElementById('c' + i).style.backgroundColor == '#BF00FF') {
+			ocument.getElementById('c' + i).style.backgroundColor = '#DA81F5';
+		}
+	}
 	if (this.corners[0] !== 100 && this.corners[1] !== 100) {
 		var a = this.corners[0];
 		var b = this.corners[1];
