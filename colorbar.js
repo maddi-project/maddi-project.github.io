972834376ff4d5d952c80aa6a678f10b56f2876e
maddi-project <69698429+maddi-project@users.noreply.github.com>
2020-11-08 15:55:53 +0100

    Create colorbar.js

---
+++ colorbar.js
@@ -0,0 +1,176 @@
+function onload() {
+	alert("WARNING\nIf you continue you accept to cite any results obtained using this website.");
+	//document.getElementById("defaultOpen").click();
+	//document.getElementById("defaultOpen1").click();
+}
+
+function draw() {
+  var c = document.getElementById('canvas');
+  var ctx = c.getContext("2d");
+  var img = document.getElementById('output');
+  var val = document.getElementById("zoom-value");
+  var xs = document.getElementById("x-value");
+  var ys = document.getElementById("y-value");
+  var zoom = val.value;
+  var xp = xs.value/100;
+  var yp = ys.value/100;
+  var proportion = img.naturalWidth/img.naturalHeight;
+  var w = 490*proportion*zoom;
+  var h = 490*zoom;
+  var x = (744-w)*xp;
+  var y = (490-h)*yp;
+  ctx.drawImage(img,x,y,w,h);
+};
+
+var loadFile = function(event) {
+	var image = document.getElementById('output');
+	image.src = URL.createObjectURL(event.target.files[0]);
+};
+function update_slider() {
+	var slider = document.getElementById("zoom");
+	var number = document.getElementById("zoom-value");
+	number.value = slider.value;
+};
+function update_number() {
+	var slider = document.getElementById("zoom");
+	var number = document.getElementById("zoom-value");
+	slider.value = number.value;
+};
+function update_x() {
+	var slider = document.getElementById("x");
+	var number = document.getElementById("x-value");
+	number.value = slider.value;
+};
+function update_xnumber() {
+	var slider = document.getElementById("x");
+	var number = document.getElementById("x-value");
+	slider.value = number.value;
+};
+function update_y() {
+	var slider = document.getElementById("y");
+	var number = document.getElementById("y-value");
+	number.value = slider.value;
+};
+function update_ynumber() {
+	var slider = document.getElementById("y");
+	var number = document.getElementById("y-value");
+	slider.value = number.value;
+};
+
+function update_slider1() {
+	update_slider();
+	draw();
+};
+function update_number1() {
+	update_number();
+	draw();
+};
+function update_x1() {
+	update_x();
+	draw();
+};
+function update_xnumber1() {
+	update_xnumber();
+	draw();
+};
+function update_y1() {
+	update_y();
+	draw();
+};
+function update_ynumber1() {
+	update_ynumber();
+	draw();
+};
+
+function draw1(i,j) {
+  	var c = document.getElementById('canvas2');
+  	var ctx = c.getContext("2d");
+  	var img = document.getElementById('output');
+  	var val = document.getElementById("zoom-value");
+	var xs = document.getElementById("x-value");
+  	var ys = document.getElementById("y-value");
+	var dim = document.getElementById("dimension");
+  	var zoom = val.value;
+  	var xp = xs.value/100;
+  	var yp = ys.value/100;
+  	var proportion = img.naturalWidth/img.naturalHeight;
+  	var w = 490*proportion*zoom;
+  	var h = 490*zoom;
+  	var x = (744-w)*xp;
+  	var y = (490-h)*yp;
+	var posx = ((-1*x)+1.1*744/12.3+i*0.9*744/12.3)*img.naturalWidth/(490*proportion)/zoom;
+	var posy = ((-1*y)+0.7*490/8.1+j*0.9*490/8.1)*img.naturalHeight/490/zoom;
+  	ctx.drawImage(img,posx,posy,dim.value,dim.value,11+i*61,10+j*60,50,50);
+	
+	var RGB
+	RGB = ctx.getImageData(11+i*61,10+j*60,50,50);
+	//RGB = ctx.getImageData(1.1*744/12.3+i*0.9*744/12.3,0.7*490/8.1+j*0.9*490/8.1,dim.value,dim.value);
+	return RGB;
+};
+function crop1() {
+	var Data = [];
+	for (i = 0; i < 12; i++) {
+  		for (j = 0; j < 8; j++) {
+			Data[j*12+i] = draw1(i,j);
+		}
+	}
+	return Data
+};
+function crop() {
+	crop1();
+	window.scrollTo(0,695);
+};
+function get_data1() {
+	var imgData = crop1();
+	var RGB_values = [];
+	for (var i = 0; i < 96; i++) {
+		var Rp = 0;
+		var Gp = 0;
+		var Bp = 0;
+		for (var j = 0; j < imgData[i].data.length; j+=4) {
+  			Rp = Rp + imgData[i].data[j];
+  			Gp = Gp + imgData[i].data[j+1]
+  			Bp = Bp + imgData[i].data[j+2]
+  		}
+		RGB_values[i] = Rp/(imgData[i].data.length/4);
+		RGB_values[i+96] = Gp/(imgData[i].data.length/4);
+		RGB_values[i+192] = Bp/(imgData[i].data.length/4);
+	}
+	return RGB_values	
+};
+function roundTo(n, digits) {
+	if (digits === undefined) {
+       	digits = 0;
+     	}
+
+     	var multiplicator = Math.pow(10, digits);
+     	n = parseFloat((n * multiplicator).toFixed(11));
+     	var test =(Math.round(n) / multiplicator);
+     	return +(test.toFixed(digits));
+}
+function get_data() {
+	RGB = get_data1();
+	for (var i = 0; i < 96; i++) {
+		document.getElementById('R' + i).innerHTML = roundTo(RGB[i], 2);
+		document.getElementById('G' + i).innerHTML = roundTo(RGB[i+96], 2);
+		document.getElementById('B' + i).innerHTML = roundTo(RGB[i+192], 2);
+	}
+	// Get the element with id="defaultOpen" and click on it
+	document.getElementById("defaultOpen").click();
+	window.scrollTo(0,1390);
+};
+function table_to_array(table_id) {
+        var myData = document.getElementById(table_id).rows
+        //console.log(myData)
+        var my_liste = []
+        for (var i = 0; i < myData.length; i++) {
+        	var el = myData[i].children
+                var my_el = []
+                for (var j = 0; j < el.length; j++) {
+                	my_el.push(el[j].innerText);
+                }
+                my_liste.push(my_el)
+
+        }
+        return my_liste
+};
